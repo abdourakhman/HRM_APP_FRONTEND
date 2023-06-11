30c3a9900ee8713c95587229d2b16550e8eb7906
abdourakhman <abdourakhman920@gmail.com>
2023-06-11 15:22:21 +0200

    list of department

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DepartmentsComponent } from './department/departments.component';
+import { DepartmentlistComponent } from './department/departmentlist/departmentlist.component';
 
+
+const appRoutes:Routes = [
+  {path:'dashboard', component:DashboardComponent},
+  {path:'departments',component:DepartmentsComponent},
+  {path:'',component:DashboardComponent},
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    DashboardComponent,
+    DepartmentsComponent,
+    DepartmentlistComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/department/departments.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-department',
+  templateUrl: './departments.component.html',
+  styleUrls: ['./departments.component.css']
+})
+export class DepartmentsComponent {
+
+}
