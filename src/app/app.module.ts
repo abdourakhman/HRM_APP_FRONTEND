a39186a2ff93749c2e1d1c23b41db120e4a17d17
abdourakhman <abdourakhman920@gmail.com>
2023-06-11 21:49:20 +0200

    list of employee

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DepartmentsComponent } from './department/departments.component';
 import { DepartmentlistComponent } from './department/departmentlist/departmentlist.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { EmployeeListComponent } from './employee/employee-list/employee-list.component';
+import { EmployeeProfilComponent } from './employee/employee-profil/employee-profil.component';
 
 
 const appRoutes:Routes = [
   {path:'dashboard', component:DashboardComponent},
   {path:'departments',component:DepartmentsComponent},
+  {path:'employees',component:EmployeeComponent},
   {path:'',component:DashboardComponent},
 ]
 @NgModule({
@@ -21,7 +25,10 @@ const appRoutes:Routes = [
     NavigationComponent,
     DashboardComponent,
     DepartmentsComponent,
-    DepartmentlistComponent
+    DepartmentlistComponent,
+    EmployeeComponent,
+    EmployeeListComponent,
+    EmployeeProfilComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/employee/employee-profil/employee-profil.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-profil',
+  templateUrl: './employee-profil.component.html',
+  styleUrls: ['./employee-profil.component.css']
+})
+export class EmployeeProfilComponent {
+
+}
