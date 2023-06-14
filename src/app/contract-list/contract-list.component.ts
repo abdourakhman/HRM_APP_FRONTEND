7b04d7f4a371881407aeeb76f6856a4b6f29f67f
abdourahmane NDIAYE <abdourakhman920@gmail.com>
2023-06-14 12:17:14 +0200

    datastate management of entity contract

--- src/app/contract-list/contract-list.component.ts
+++ src/app/contract-list/contract-list.component.ts
@@ -1,22 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription, tap } from 'rxjs';
 import { ResourceService } from '../services/resource.service';
 import { Contract } from '../models/Contract.model';
+import { DataState } from '../enumeration/DataState.enum';
 
 @Component({
   selector: 'app-contract-list',
   templateUrl: './contract-list.component.html',
   styleUrls: ['./contract-list.component.css']
 })
-export class ContractListComponent implements OnInit{
+export class ContractListComponent implements OnInit, OnDestroy{
 
   constructor(private resourceService:ResourceService){}
   
-  contracts$!: Observable<Contract[]>;
+  contracts$?: Observable<Contract[]>;
+  contractSubscription! : Subscription;
+  dataState: DataState = DataState.LOADING;
 
   ngOnInit(): void {
     this.contracts$ = this.resourceService.listContract();
+    this.contractSubscription = this.contracts$.subscribe(
+      (value)=>{console.log},
+      (error)=>this.dataState = DataState.ERROR,
+      ()=>this.dataState=DataState.COMPLETE
+    )
   }
-
+  ngOnDestroy(): void {
+    this.contractSubscription.unsubscribe();
+  }
 
 }
---
+++ src/app/enumeration/DataState.enum.ts
@@ -0,0 +1,5 @@
+export enum DataState{
+    LOADING = 'LOADING',
+    COMPLETE = 'COMPLETE',
+    ERROR = 'ERROR' 
+}
